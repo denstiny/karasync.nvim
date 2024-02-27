c9e552bc389e6dd67dad7de9e50e95736a71e355
denstiny <2254228017@qq.com>
2024-02-27 17:52:38 +0800

    feat: init

---
+++ lua/karasync/init.lua
@@ -0,0 +1,9 @@
+-- @author      : denstiny (2254228017@qq.com)
+-- @file        : init
+-- @created     : 星期二 2月 27, 2024 17:52:17 CST
+-- @github      : https://github.com/denstiny
+-- @blog        : https://denstiny.github.io
+
+local M = {}
+
+return M
